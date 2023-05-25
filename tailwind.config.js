a6ab94a4dbd79ef93ec74b10da2be0f6ec404243
Orex <adrena.orex@proton.me>
2023-05-25 10:23:07 +0400

    Setup landing core content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        // background colors
+        // ADRENA Style
+        main: "#171717",
+        secondary: "#1E1D1D",
+        third: "#343232",
+        highlight: "#36538f", // RED: #C43152
+
+        // border
+        grey: "#2A2A2A",
+
+        /*
+        // GMX Style
+        main: '#101124',
+        secondary: '#16182D',
+        third: '#242842',
+        highlight: '#323FCE',
+
+        // border
+        grey: '#23263b',
+        */
+
+        // text
+        txtfade: "#8C8D97",
+        txtregular: "#E5E6E8",
+
+        // To change leverage colors, search for:
+        // Leverage colors
       },
+    },
+    fontFamily: {
+      regular: ["regular"],
+      thin: ["thin"],
+      bold: ["bold"],
+      specialmonster: ["specialmonster"],
     },
   },
   plugins: [],
-}
+};
