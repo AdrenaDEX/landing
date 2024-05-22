e4e891588bc382ac8720ba2a02ae149dbe6d881a
Abdirahim-A <139131552+adrena-tyro@users.noreply.github.com>
2024-05-22 12:39:45 +0200

    early providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import Head from 'next/head';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://landing.adrena.xyz/'),
@@ -28,20 +29,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <Head>
-        <script id="hotjar-script">
-          {`(function(h,o,t,j,a,r){
-          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-          h._hjSettings={hjid:4990246,hjsv:6};
-          a=o.getElementsByTagName('head')[0];
-          r=o.createElement('script');r.async=1;
-          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-          a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
-        </script>
-      </Head>
-      <body>{children}</body>
-    </html>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,6 @@ import Footer from './Footer';
 import Team from './sections/Team';
 import EarlyProviders from './sections/EarlyProviders';
 
-
 export default function Home() {
   const [isHeaderLoaded, setIsHeaderLoaded] = useState(false);
   const [isBtmLoaded, setIsBtmLoaded] = useState(false);
@@ -27,78 +26,78 @@ export default function Home() {
   }, []);
 
   return (
-      <div className="flex flex-col relative">
-        {/* Add a shadow on both sides of the screen when screen is too big */}
-        <div className="absolute w-[5em] h-full z-30 left-0 bg-gradient-to-r from-[#1A293C] gradient__control" />
-        <div className="absolute w-[5em] h-full z-30 right-0 bg-gradient-to-l from-[#1A293C] gradient__control" />
+    <div className="flex flex-col relative">
+      {/* Add a shadow on both sides of the screen when screen is too big */}
+      <div className="absolute w-[5em] h-full z-30 left-0 bg-gradient-to-r from-[#1A293C] gradient__control" />
+      <div className="absolute w-[5em] h-full z-30 right-0 bg-gradient-to-l from-[#1A293C] gradient__control" />
 
-        <Header isLoaded={isLoaded} />
+      <Header isLoaded={isLoaded} />
 
-        <Hero isLoaded={isLoaded} setIsHeaderLoaded={setIsHeaderLoaded} />
+      <Hero isLoaded={isLoaded} setIsHeaderLoaded={setIsHeaderLoaded} />
 
-        <div className="relative">
-          <Image
-            src={sepImg}
-            alt="separator"
-            className="absolute fade-in-3 top-[-20px] sm:top-[-1em] lg:top-[-2em] scale-[3] sm:scale-[2.5] md:scale-[2] lg:scale-[1]"
-          />
-        </div>
-
-        <Trading className="" />
-
-        <div className="relative">
-          <Image
-            src={sepImg}
-            alt="separator"
-            className="absolute fade-in-3 scale-[3] sm:scale-[2.5] md:scale-[2] lg:scale-[1]"
-          />
-        </div>
-
-        <TwoToken className="mt-[1em] lg:mt-[3em]" isLoaded={isLoaded} />
-
-        <div className="relative">
-          <Image
-            src={sepImg}
-            alt="separator"
-            className="absolute fade-in-3 scale-[3] sm:scale-[2.5] md:scale-[2] lg:scale-[1] top-[-1.5em] z-20"
-          />
-        </div>
-
-        <Community
-          className="mt-[3em] lg:mt-[6em]"
-          setIsBtmLoaded={setIsBtmLoaded}
-          isLoaded={isLoaded}
+      <div className="relative">
+        <Image
+          src={sepImg}
+          alt="separator"
+          className="absolute fade-in-3 top-[-20px] sm:top-[-1em] lg:top-[-2em] scale-[3] sm:scale-[2.5] md:scale-[2] lg:scale-[1]"
         />
-
-        <div className="relative">
-          <Image
-            src={sepImg}
-            alt="separator"
-            className="absolute fade-in-3 top-[-20px] sm:top-[-1em] lg:top-[-2em] scale-[3] sm:scale-[2.5] md:scale-[2] lg:scale-[1]"
-          />
-        </div>
-
-        <FeeDistribution className="-z-10 mb-14" />
-
-        <div className="relative">
-          <Image
-            src={sepImg}
-            alt="separator"
-            className="absolute fade-in-3 top-[-20px] sm:top-[-1em] lg:top-[-2em] scale-[3] sm:scale-[2.5] md:scale-[2] lg:scale-[1]"
-          />
-        </div>
-
-        <Team className="pt-14" />
-
-        <div className="w-full h-[1px] mb-3 bg-gradient-to-r from-[#1A2A3D] via-[#2B3A55] to-[#1A2A3D]" />
-
-        <EarlyProviders className="pt-14 pb-8" />
-
-        <div className="w-full h-[1px] mb-3 bg-gradient-to-r from-[#1A2A3D] via-[#2B3A55] to-[#1A2A3D]" />
-
-        {/* <FAQ /> */}
-        {/*<div className="w-full h-[1px] mb-3 bg-gradient-to-r from-[#1A2A3D] via-[#2B3A55] to-[#1A2A3D]" />*/}
-        <Footer />
       </div>
+
+      <Trading className="" />
+
+      <div className="relative">
+        <Image
+          src={sepImg}
+          alt="separator"
+          className="absolute fade-in-3 scale-[3] sm:scale-[2.5] md:scale-[2] lg:scale-[1]"
+        />
+      </div>
+
+      <TwoToken className="mt-[1em] lg:mt-[3em]" isLoaded={isLoaded} />
+
+      <div className="relative">
+        <Image
+          src={sepImg}
+          alt="separator"
+          className="absolute fade-in-3 scale-[3] sm:scale-[2.5] md:scale-[2] lg:scale-[1] top-[-1.5em] z-20"
+        />
+      </div>
+
+      <Community
+        className="mt-[3em] lg:mt-[6em]"
+        setIsBtmLoaded={setIsBtmLoaded}
+        isLoaded={isLoaded}
+      />
+
+      <div className="relative">
+        <Image
+          src={sepImg}
+          alt="separator"
+          className="absolute fade-in-3 top-[-20px] sm:top-[-1em] lg:top-[-2em] scale-[3] sm:scale-[2.5] md:scale-[2] lg:scale-[1]"
+        />
+      </div>
+
+      <FeeDistribution className="-z-10 mb-14" />
+
+      <div className="relative">
+        <Image
+          src={sepImg}
+          alt="separator"
+          className="absolute fade-in-3 top-[-20px] sm:top-[-1em] lg:top-[-2em] scale-[3] sm:scale-[2.5] md:scale-[2] lg:scale-[1]"
+        />
+      </div>
+
+      <Team className="pt-14" />
+
+      <div className="w-full h-[1px] mb-3 bg-gradient-to-r from-[#1A2A3D] via-[#2B3A55] to-[#1A2A3D]" />
+
+      <EarlyProviders className="pt-14 pb-8" />
+
+      <div className="w-full h-[1px] mb-3 bg-gradient-to-r from-[#1A2A3D] via-[#2B3A55] to-[#1A2A3D]" />
+
+      {/* <FAQ /> */}
+      {/*<div className="w-full h-[1px] mb-3 bg-gradient-to-r from-[#1A2A3D] via-[#2B3A55] to-[#1A2A3D]" />*/}
+      <Footer />
+    </div>
   );
 }
